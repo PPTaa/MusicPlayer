4bf8c75a490f9a34ed01268efa0015c899b4735e
PPTaa <chobo05@naver.com>
2021-02-21 22:54:28 +0900

    add track Manager

--- MusicPlayer/Track/TrackManager.swift
+++ MusicPlayer/Track/TrackManager.swift
@@ -17,25 +17,57 @@ class TrackManager {
     
     // 생성자 정의하기
     init() {
+        let tracks = loadTracks()
+        self.tracks = tracks
+        self.albums = loadAlbums(tracks: tracks)
+        self.todayTrack = self.tracks.randomElement()
     }
     
     // 트랙 로드하기
     func loadTracks() -> [AVPlayerItem] {
-        return []
+        //파일을 읽어서 AVPlayerItem 만들기
+        let urls = Bundle.main.urls(forResourcesWithExtension: "mp3", subdirectory: nil) ?? []
+        
+//        var items: [AVPlayerItem] = []
+//        for url in urls {
+//            let item = AVPlayerItem(url: url)
+//            items.append(item)
+//        }
+        
+        let items = urls.map{url -> AVPlayerItem in
+            return AVPlayerItem(url: url)
+        }
+        
+        return items
     }
     
     // 인덱스에 맞는 트랙 로드하기
     func track(at index:Int) -> Track? {
-        return nil
+        let playerItem = tracks[index]
+        // playerItem > track
+        let track = playerItem.convertToTrack()
+        return track
     }
     
     // 앨범 로딩메소드 구현
     func loadAlbums(tracks: [AVPlayerItem]) -> [Album] {
-        return []
+        
+        let trackList: [Track] = tracks.compactMap { $0.convertToTrack() }
+        // trackList를 활용하여 딕셔너리 제작
+        let albumDics = Dictionary(grouping: trackList, by: { (track) in track.albumName })
+        var albums: [Album] = []
+        for (key, value) in albumDics {
+            let title = key
+            let tracks = value
+            let album = Album(title: title, tracks: tracks)
+            albums.append(album)
+        }
+        return albums
     }
     
-    // 오늘의 트랙 랜덤으로 선택
+    // 오늘의 트랙 랜덤으로 재 선택
     func loadOtherTodaysTrack(){
+        self.todayTrack = self.tracks.randomElement()
         
     }
 }
